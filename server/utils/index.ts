afd00c8b7633f0510ee81b5a10d93d750d4f002d
ShadyLTD <shadydroid@gmail.com>
2025-11-02 23:14:27 -0500

    🔁 Wire progressive levels into dataLoader exports

---
+++ server/utils/index.ts
@@ -0,0 +1,4 @@
+import { syncUpgrades, syncCharacters } from "./dataLoader";
+import { syncLevels, getLevelsFromMemory, getLevelFromMemory } from "./levelsProgressive";
+
+export { syncUpgrades, syncCharacters, syncLevels, getLevelsFromMemory, getLevelFromMemory };
